436f8e794388cd7368c1814e925092b1f47048ee
STDin <diego.rodriguez.gomez@stdin.cl>
2023-04-05 18:27:59 -0400

    2.0.4

--- src/Format.php
+++ src/Format.php
@@ -11,5 +11,18 @@ class Format {
 		$decimals = strlen(isset($number[1])?$number[1]:'');
 		return $currentSymbol.number_format($v,$decimals,',','.');
 	}
+	public static function normalize($name){
+		Parse::decode($name); 
+		$name = strtolower($name);
+		$name = str_replace('á', 'a', $name);
+		$name = str_replace('é', 'e', $name);
+		$name = str_replace('í', 'i', $name);
+		$name = str_replace('ó', 'o', $name);
+		$name = str_replace('ú', 'u', $name);
+		$name = str_replace('ñ', 'n', $name);
+		$name = str_replace(' ', '-', $name);
+		$name = preg_replace("/[^a-z0-9-.]+/i", '', $name);
+		return $name;
+	}
 }
 ?>
